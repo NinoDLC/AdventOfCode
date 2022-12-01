f9efc2437c8e5f11f412f4e54f6e2794c5bb8625
Nino <anthony@delcey.fr>
2022-12-01 13:33:20 +0100

    Update logMeasureTime to autoprint duration value

--- src/main/kotlin/utils/MeasureTimeLogger.kt
+++ src/main/kotlin/utils/MeasureTimeLogger.kt
@@ -1,17 +1,12 @@
 package utils
 
 import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.milliseconds
 
 fun logMeasureTime(block: () -> Unit) {
     measureTimeMillis {
         block.invoke()
-    }.let {
-        println(
-            if (it < 1_000) {
-                "Took ${it}ms to run"
-            } else {
-                "Took ${it / 1_000}s to run"
-            }
-        )
+    }.milliseconds.let {
+        println("Took $it to run")
     }
 }
