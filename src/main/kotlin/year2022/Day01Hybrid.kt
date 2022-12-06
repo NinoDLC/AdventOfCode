65c86bf6752ae36fb59a7bad0e05adacf9ff785b
Nino <anthony@delcey.fr>
2022-12-06 22:42:19 +0100

    Add fully imperative and hybrid style for a beginner-friendly introduction to functional programming

---
+++ src/main/kotlin/year2022/Day01Hybrid.kt
@@ -0,0 +1,60 @@
+package year2022
+
+import utils.logMeasureTime
+import java.io.File
+
+class Day01Hybrid {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lines = File("data/2022/day01.txt").readLines()
+
+            println("=== Part One ===")
+            logMeasureTime {
+                Day01Hybrid().partOne(lines)
+            }
+            println()
+
+            println("=== Part Two ===")
+            logMeasureTime {
+                Day01Hybrid().partTwo(lines)
+            }
+            println()
+        }
+    }
+
+    private fun partOne(lines: List<String>) {
+        var maxCalories = 0
+        var currentCalories = 0
+
+        for (line in lines) {
+            if (line.isEmpty()) {
+                if (currentCalories > maxCalories) {
+                    maxCalories = currentCalories
+                }
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+
+        println(maxCalories)
+    }
+
+    private fun partTwo(lines: List<String>) {
+        val calories = mutableListOf<Int>()
+        var currentCalories = 0
+
+        for (line in lines) {
+            if (line.isEmpty()) {
+                calories.add(currentCalories)
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+
+        println(calories.sortedDescending().take(3).sum())
+    }
+}
---
+++ src/main/kotlin/year2022/Day01Imperative.kt
@@ -0,0 +1,76 @@
+package year2022
+
+import utils.logMeasureTime
+import java.io.File
+
+class Day01Imperative {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lines = File("data/2022/day01.txt").readLines()
+
+            println("=== Part One ===")
+            logMeasureTime {
+                Day01Imperative().partOne(lines)
+            }
+            println()
+
+            println("=== Part Two ===")
+            logMeasureTime {
+                Day01Imperative().partTwo(lines)
+            }
+            println()
+        }
+    }
+
+    private fun partOne(lines: List<String>) {
+        var maxCalories = 0
+        var currentCalories = 0
+
+        for (line in lines) {
+            if (line.isEmpty()) {
+                if (currentCalories > maxCalories) {
+                    maxCalories = currentCalories
+                }
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+
+        println(maxCalories)
+    }
+
+    private fun partTwo(lines: List<String>) {
+        var maxCalories1 = 0
+        var maxCalories2 = 0
+        var maxCalories3 = 0
+        var currentCalories = 0
+
+        for (line in lines) {
+            if (line.isEmpty()) {
+                when {
+                    currentCalories > maxCalories1 -> {
+                        maxCalories3 = maxCalories2
+                        maxCalories2 = maxCalories1
+                        maxCalories1 = currentCalories
+                    }
+
+                    currentCalories > maxCalories2 -> {
+                        maxCalories3 = maxCalories2
+                        maxCalories2 = currentCalories
+                    }
+
+                    currentCalories > maxCalories3 -> maxCalories3 = currentCalories
+                }
+
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+
+        println(maxCalories1 + maxCalories2 + maxCalories3)
+    }
+}
