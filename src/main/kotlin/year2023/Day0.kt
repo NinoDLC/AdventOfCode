c728aba5a1d1c4125fb34df892cf8761c7476e00
Nino <anthony@delcey.fr>
2023-12-01 11:55:31 +0100

    2023 - Day 0

---
+++ src/main/kotlin/year2023/Day0.kt
@@ -0,0 +1,34 @@
+package year2023
+
+import utils.getPuzzleInput
+import utils.logMeasureTime
+
+class Day0 {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lines = getPuzzleInput(this)
+
+            println("=== Part One ===")
+            logMeasureTime {
+                Day0().partOne(lines)
+            }
+            println()
+
+            println("=== Part Two ===")
+            logMeasureTime {
+                Day0().partTwo(lines)
+            }
+            println()
+        }
+    }
+
+    private fun partOne(lines: List<String>) {
+        println(lines)
+    }
+
+    private fun partTwo(lines: List<String>) {
+        println(lines)
+    }
+}
